a25d7436e4dfa2d53df4ff6e4472f09a1a8ce18b
abdelkawi <mohamed.abdelkawi@rytalo.com>
2022-06-26 23:42:59 +0200

    InitProject

---
+++ app/src/main/java/com/example/jd3tvplayground/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jd3tvplayground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+  }
+}
