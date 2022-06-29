53647a80df3f9bb6780ace2f33bee85507e9cb07
abdelkawi <mohamed.abdelkawi@rytalo.com>
2022-06-29 14:07:31 +0200

    ProjectSetup

--- app/src/main/java/com/example/jd3tvplayground/domain/ApiResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jd3tvplayground.domain
-
-data class ApiResponse<T>(
-     val data: T?,
-     val errors:List<String>?
-)
--- app/src/main/java/com/example/jd3tvplayground/domain/home/DomainOfWebClientResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.jd3tvplayground.domain.home
-
-//
-// Created by Dina Mounib on 6/28/22.
-//
-class DomainOfWebClientResponse {
-}
