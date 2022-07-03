3c6d650853c126fef125a17c6cc984d725c685d2
abdelkawi <mohamed.abdelkawi@rytalo.com>
2022-07-03 16:00:02 +0200

    ProjectSetup

--- app/src/main/java/com/genwin/jd3tv/screens/home/presentation/MainActivity.kt
+++ app/src/main/java/com/genwin/jd3tv/screens/home/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.genwin.jd3tv.screens.home.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -16,11 +17,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.genwin.jd3tv.R
+import com.genwin.jd3tv.common.Result.Error
+import com.genwin.jd3tv.common.Result.Success
 import com.genwin.jd3tv.screens.home.data.DataItem
 import com.genwin.jd3tv.screens.home.domain.entity.HomeSection
 import com.genwin.jd3tv.screens.home.domain.entity.SectionType.Card
@@ -28,30 +32,34 @@ import com.genwin.jd3tv.screens.home.domain.entity.SectionType.CardWithTitle
 import com.genwin.jd3tv.screens.home.domain.entity.SectionType.Contest
 import com.genwin.jd3tv.screens.home.domain.entity.SectionType.ViewPager
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+  private val homeViewModel:HomeViewModel by viewModels()
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    setContent {
-      main()
+    lifecycleScope.launch {
+      val res = homeViewModel.getHomeDetails()
+      when(res){
+        is Error -> {
+        }
+        is Success -> {
+          setContent {
+            main(res.data)
+          }
+        }
+      }
     }
   }
-  @Preview
   @Composable
-  fun main(){
+  fun main(sections:List<HomeSection>){
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "home") {
       composable("profile") { profile() }
       composable("home") {
-        home(listOf(
-          HomeSection("section 1",Card, items = listOf(DataItem("section 1"),DataItem("section 1"))),
-            HomeSection("section 2",ViewPager, items = listOf(DataItem("section 1"),DataItem("section 1"))),
-            HomeSection("section 3",Contest, items = listOf(DataItem("section 1"),DataItem("section 1"))),
-            HomeSection("section 4",CardWithTitle, items = listOf(DataItem("section 1"),DataItem("section 1")))
-          ),
+        home(sections,
           navController) }
     }
   }
@@ -78,7 +86,7 @@ class MainActivity : ComponentActivity() {
           when(section.type){
             Card -> SectionCard(imageRes = R.drawable.image_1)
             ViewPager -> SectionCard(imageRes = R.drawable.image_2)
-            CardWithTitle -> Text(item.ref?:"No title", modifier = Modifier.padding(all = 8.dp))
+            CardWithTitle -> Text(item.ref ?: "No title", modifier = Modifier.padding(all = 8.dp))
             Contest -> SectionCard(imageRes = R.drawable.image_1)
           }
 
