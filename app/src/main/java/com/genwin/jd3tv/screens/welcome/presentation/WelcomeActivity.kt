f326cd33315dc908140cfad11a75ae5d86176238
Dina Mounib <dina@rytalo.com>
2022-07-03 15:06:38 +0200

    welcome activity

---
+++ app/src/main/java/com/genwin/jd3tv/screens/welcome/presentation/WelcomeActivity.kt
@@ -0,0 +1,16 @@
+package com.genwin.jd3tv.screens.welcome.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.jd3tvplayground.R
+
+//
+// Created by Dina Mounib on 7/3/22.
+//
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
