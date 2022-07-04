255351b91f7427296623a93b17e93f2440bfa9b2
Dina Mounib <dina@rytalo.com>
2022-07-04 10:01:07 +0200

    splash

--- app/src/main/java/com/genwin/jd3tv/screens/splash/domain/ApiService.kt
+++ app/src/main/java/com/genwin/jd3tv/screens/splash/domain/ApiService.kt
@@ -1,6 +1,7 @@
-package com.genwin.jd3tv.screens.splash.domain.home
+package com.genwin.jd3tv.screens.splash.domain
 
 import com.genwin.jd3tv.common.ApiResponse
+import com.genwin.jd3tv.screens.splash.domain.home.DomainOfWebClientResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
