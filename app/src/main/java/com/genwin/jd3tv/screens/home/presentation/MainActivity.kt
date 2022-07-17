844581bfdc6558c9718d897e1d18cdade8a104dc
Dina Mounib <dina@rytalo.com>
2022-07-17 12:54:35 +0200

    drawer from right

--- app/src/main/java/com/genwin/jd3tv/screens/home/presentation/MainActivity.kt
+++ app/src/main/java/com/genwin/jd3tv/screens/home/presentation/MainActivity.kt
@@ -1,76 +1,44 @@
 package com.genwin.jd3tv.screens.home.presentation
 
 import android.os.Bundle
+import android.util.LayoutDirection
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import coil.size.Scale
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.genwin.jd3tv.R
-import com.genwin.jd3tv.R.font
 import com.genwin.jd3tv.common.Result.Error
 import com.genwin.jd3tv.common.Result.Success
 import com.genwin.jd3tv.screens.home.domain.entity.BottomTab
 import com.genwin.jd3tv.screens.home.domain.entity.HomeSection
-import com.genwin.jd3tv.screens.home.domain.entity.SectionType
-import com.genwin.jd3tv.screens.home.domain.entity.SectionType.Card
-import com.genwin.jd3tv.screens.home.domain.entity.SectionType.CardWithTitle
-import com.genwin.jd3tv.screens.home.domain.entity.SectionType.ViewPager
 import com.genwin.jd3tv.screens.splash.presentation.SplashViewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
@@ -110,14 +78,20 @@ class MainActivity : ComponentActivity() {
                   }
                 }
                 setContent {
-                  Scaffold(
-                    drawerContent = {
-                      Profile()
-                    },
-                    content = { Main(sections = sections, tabs = tabs.map { BottomTab(it, it) }.subList(0, 4)) },
-                    drawerBackgroundColor = Color(0xE6460383),
-                    contentColor = Companion.White
-                  )
+                  CompositionLocalProvider(LocalLayoutDirection provides androidx.compose.ui.unit.LayoutDirection.Rtl) {
+                    ModalDrawer(
+                      drawerContent = {
+                        Profile()
+                      },
+                      content = {
+                        Main(
+                          sections = sections,
+                          tabs = tabs.map { BottomTab(it, it) }.subList(0, 4)
+                        )
+                      },
+                      drawerBackgroundColor = Color(0xE6460383)
+                    )
+                  }
                 }
               }
             }
