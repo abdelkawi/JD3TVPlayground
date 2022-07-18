0f5de3710dae7e215cdba7a63690975c0e71ec38
Dina Mounib <dina@rytalo.com>
2022-07-18 15:22:54 +0200

    splash modify

--- app/src/main/java/com/genwin/jd3tv/screens/splash/presentation/SplashActivity.kt
+++ app/src/main/java/com/genwin/jd3tv/screens/splash/presentation/SplashActivity.kt
@@ -36,9 +36,9 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-//        if (sharedPreference.getUserName().isNotEmpty()) {
-//            login(sharedPreference.getUserName(), sharedPreference.getPassword())
-//        }else
+        if (sharedPreference.getUserName().isNotEmpty()) {
+            login(sharedPreference.getUserName(), sharedPreference.getPassword())
+        }else
             startActivity(Intent(this@SplashActivity, WelcomeActivity::class.java))
     }
 
